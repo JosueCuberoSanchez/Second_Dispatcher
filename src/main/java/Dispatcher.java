c46fc36b2a16e93a9196a358fa377330e90860cb
Mariana <mariana.abellan@ucr.ac.cr>
2017-11-10 00:53:12 -0600

    Falta que arregle la tabla

--- src/main/java/Dispatcher.java
+++ src/main/java/Dispatcher.java
@@ -11,16 +11,20 @@ public class Dispatcher {
     public Map<String,Pair<String,String>> oneToOneRelation;
 
     public Dispatcher(){
-        this.ipTable = new HashMap<String,IpData>();
+        this.ipTable = new HashMap<String,IpData>(); //silvia
+
                         //key is network ip      computer ip                                 through
-        this.ipTable.put("12.0.0.0",new IpData("127.0.0.1", "12.0.0.8", "12.0.0.7", 0,8888));//banderas
-        this.ipTable.put("200.5.0.0",new IpData("192.168.100.16", "200.5.0.2", "12.0.0.7",1,0));//paletas
-        this.ipTable.put("140.90.0.0",new IpData("192.168.100.16", "12.0.0.8", "12.0.0.7", 2,0));//bolinchas
+        this.ipTable.put("12.0.0.0",new IpData("127.0.0.1", "12.0.0.8", "165.8.6.25", 0,8888));//banderas
+        this.ipTable.put("200.5.0.0",new IpData("192.168.100.16", "200.5.0.2", "165.8.6.25",1,0));//paletas
+        this.ipTable.put("140.90.0.0",new IpData("192.168.100.16", "12.0.0.8", "165.8.6.25", 2,0));//bolinchas
+
+        this.ipTable.put("200.5.0.0",new IpData("192.168.100.16", "200.5.0.2", "165.8.0.6",3,0));//paletas
         this.ipTable.put("201.6.0.0",new IpData("192.168.100.16", "12.0.0.8", "165.8.0.6", 1,0));//legos
         this.ipTable.put("25.0.0.0",new IpData("192.168.100.16", "12.0.0.8", "165.8.0.6", 0,5555));//luces
+
         this.oneToOneRelation = new HashMap<String, Pair<String,String>>();
-        this.oneToOneRelation.put("165.8.0.6",new Pair<String,String>("127.0.0.1","6666"));//cambiar luego!
-        this.oneToOneRelation.put("165.8.25.6",new Pair<String,String>("127.0.0.1","5555"));
+        this.oneToOneRelation.put("165.8.0.6",new Pair<String,String>("127.0.0.1","7777"));
+        this.oneToOneRelation.put("165.8.6.25",new Pair<String,String>("127.0.0.1","5555"));
     }
 
     public void startDispatcher(){
